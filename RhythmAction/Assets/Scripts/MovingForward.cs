97aa4e00baa9b77ca419df17463c1ac0b226be41
Danny <shortieofcheese@gmail.com>
2015-10-22 15:22:11 -0400

    Note Presses

---
+++ RhythmAction/Assets/Scripts/MovingForward.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingForward : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		transform.position -= transform.forward * Time.deltaTime * speed;
+
+	}
+}
---
+++ RhythmAction/Assets/Scripts/NoteLogic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteLogic : MonoBehaviour {
+
+	// Use this for initialization
+
+	bool click = false;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(click){
+			if(Input.GetMouseButtonDown(1) && this.gameObject.CompareTag ("RightNote")){
+				Destroy(this.gameObject);
+			}
+			if(Input.GetMouseButtonDown(0) && this.gameObject.CompareTag ("LeftNote")){
+				Destroy(this.gameObject);
+			}
+		}
+	
+	}
+
+	void Clickable () {
+		click = true;
+	}
+}
